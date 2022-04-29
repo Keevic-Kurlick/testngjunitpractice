9a12a73f7a2ae9e1a4777063aaa8754e38318674
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-04-29 18:54:50 +0300

    tests on bound values

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.service;
+
+import org.testng.annotations.*;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    private CashbackHackService service;
+
+    @BeforeSuite
+    public void setUp() {
+        service = new CashbackHackService();
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        service = null;
+    }
+
+    @Test
+    public void shouldRemainIfUnderBound() {
+        int actual = service.remain(900);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemainIfEqualsBound() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemainIfAboveBound() {
+        int actual = service.remain(1100);
+        int expected = -100;
+        assertEquals(actual, expected);
+    }
+}
